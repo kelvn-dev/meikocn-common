4de12cdd38f7bb80d0ad7f59d82576c528c59d58
Kelvin Vu <kelvn.developer@gmail.com>
2025-06-15 16:38:28 +0700

    init pipeline to publish maven package

---
+++ src/main/java/com/meikocn/common/MeikocnCommonApplication.java
@@ -0,0 +1,13 @@
+package com.meikocn.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeikocnCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeikocnCommonApplication.class, args);
+    }
+
+}
